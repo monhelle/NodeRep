350ec5c24e1b971c76ee86d71ff87591bb309d39
Monica Stefferud-Helle <monicastef@Monicas-MacBook-Pro.local>
2026-09-18 12:13:12 +0200

    adding mongodb

--- app.js
+++ app.js
@@ -1,59 +1,83 @@
 const express = require("express"); //importert express server
+const argon2 = require("argon2");
+const mongoose = require("mongoose");
+
+const mongodb = mongoose.connect("mongodb://localhost:27017/coffee");
+
+//databasemodeller
+const User = require("./models/User")
+
+
+
 
 const app = express(); //vi lager en app
 
 app.set("view engine", "ejs"); //bruker embedded javascript som view engine
-app.use(express.urlencoded({extended: true})); //trengs for å sende info i req.body fra frontend
+app.use(express.urlencoded({ extended: true })); //trengs for å sende info i req.body fra frontend
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    // res.send("<h1>I need coffee</h1><p>lorem ipsum</p>")
-    res.render("index"); //trenger ikke filtype
-})
+  // res.send("<h1>I need coffee</h1><p>lorem ipsum</p>")
+  res.render("index"); //trenger ikke filtype
+});
 
 app.get("/americano", (req, res) => {
-    res.render("americano")
-})
+  res.render("americano");
+});
 
 app.get("/latte", (req, res) => {
-    res.render("latte")
-})
+  res.render("latte");
+});
 
 app.get("/cappuchino", (req, res) => {
-    res.render("cappuchino")
-})
+  res.render("cappuchino");
+});
 
 app.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 app.get("/registrer", (req, res) => {
-    res.render("registrer")
-})
+  res.render("registrer");
+});
 
 app.post("/", (req, res) => {
-    console.log(req.body);
-    const { kaffe, antall, size } = req.body;
-    res.send(`Takk for at du valgte ${antall} ${size} ${kaffe}`)
-})
+  console.log(req.body);
+  const { kaffe, antall, size } = req.body;
+  res.send(`Takk for at du valgte ${antall} ${size} ${kaffe}`);
+});
 
 app.post("/login", (req, res) => {
-    const {email, passord} = req.body;
-    res.send(`Din epost og passord er ${email} ${passord}`)
-})
+  const { email, passord } = req.body;
+  res.send(`Din epost og passord er ${email} ${passord}`);
+});
+
+app.post("/registrer", async (req, res) => {
+  //async
+  const { email, passord, gjentaPassord } = req.body;
+
+  if (passord !== gjentaPassord) {
+    res.send("passord og gjenta passord stemmer ikke overens");
+  } else {
+    // hashe passordet her
+    const hash = await argon2.hash(passord); //await
+
+    const user = User.insertOne({
+        email, 
+        passord: hash
+    })
+
+    console.log(user);
 
 
-app.post("/registrer", (req, res) => {
-    const {email, passord, gjentaPassord} = req.body;
-    if(passord !== gjentaPassord) {
-        res.send("passord og gjenta passord stemmer ikke overens")
-    } else {
-        res.redirect("/")
-    }
 
-    // res.send(`Din epost og passord er ${email} ${passord} ${gjentaPassord}`)
+    console.log(hash);
 
-})
+    res.redirect("/login");
+  }
+
+  // res.send(`Din epost og passord er ${email} ${passord} ${gjentaPassord}`)
+});
 
 app.listen(4000, () => {
-    console.log("http://localhost:4000")
+  console.log("http://localhost:4000");
 }); //vi kjører appen på port 4000
